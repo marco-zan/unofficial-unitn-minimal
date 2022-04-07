1fa4ef2241ed89c1e1adfb2b35d1a819c5e97ec5
Marco Zanon <marco.zanon@pm.me>
2022-04-07 11:22:21 +0200

    Added auto-skip enrollment and login screen

---
+++ js/auto_click_enrollment.js
@@ -0,0 +1,7 @@
+/* 
+    To click automatically to the "Continue" button on the enrollment page
+
+    This doesn't mean that you will automatically enroll to new courses, because
+    the register phase is done in "Gestione corsi online"
+*/
+document.querySelector(".continuebutton > form > button.btn.btn-primary").click()
---
+++ js/auto_click_login.js
@@ -0,0 +1,5 @@
+/* 
+    Clicks auomatically on the "login" button on the page BEFORE
+    the actual login. This page is completely unnecessary
+*/
+document.querySelector("#btn-login-unitn-it").click();
